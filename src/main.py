e6d1e8898da39e18ffd01571356eeec9d9f9bc1d
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2024-02-17 17:19:17 -0300

    feat: create customers/findByDocument endpoint

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from flask_restful import Resource, Api, reqparse
+from src.controllers.CustomerController import CustomerController
+import json
+        
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def home():
-    return "Hello World!"
+api.add_resource(CustomerController, '/customers/findByDocument')
 
 if __name__ == "__main__":
     app.run()
