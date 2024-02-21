5e8a63e1479d6acb80243f1ace7fee011881c078
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2024-02-20 22:10:32 -0300

    chore: removing frameworks from entity customer to implement customer adapter

--- src/domain/customers/Customer.py
+++ src/domain/customers/Customer.py
@@ -1,14 +1,46 @@
-from sqlalchemy import Column, Integer, String, DECIMAL, Date, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+class CustomerEntity():
+    # Meta Data
+    table_name = 'customers'
 
-planb_database = declarative_base()
+    # Columns
+    id_customer = {'column_name': 'id_customer', 'object_type': 'Column', 'data_type': 'Integer', 'primary_key': True}
+    first_name = {'column_name': 'first_name', 'object_type': 'Column', 'data_type': 'String', 'length': '45'}
+    last_name = {'column_name': 'last_name', 'object_type': 'Column', 'data_type': 'String', 'length': '45'}
+    address = {'column_name': 'address', 'object_type': 'Column', 'data_type': 'String', 'length': '150'}
+    document = {'column_name': 'document', 'object_type': 'Column', 'data_type': 'String', 'length': '11'}
+    phone_number = {'column_name': 'phone_number', 'object_type': 'Column', 'data_type': 'String', 'length': '12'}
+    birthdate = {'column_name': 'birthdate', 'object_type': 'Column', 'data_type': 'String', 'length': '45'}
 
-class CustomerEntity(planb_database):
-    __tablename__ = 'customers'
-    id_customer = Column(Integer, primary_key=True)
-    first_name = Column(String(45))
-    last_name = Column(String(45))
-    address = Column(String(150))
-    document = Column(String(11))
-    phone_number = Column(String(12))
-    birthdate = Column(String(45))
+    
+    def getLenPrecision(column_name):
+
+        try:
+            lenght = int(0 if getattr(CustomerEntity, column_name).get('length') == None else getattr(CustomerEntity, column_name).get('length'))
+            precision = int(0 if getattr(CustomerEntity, column_name).get('precision') == None else getattr(CustomerEntity, column_name).get('precision'))
+
+            lenPrecision = '%s,%s' % (len, precision) if (precision != None and precision > 0) else len if precision != None else None
+
+        except AttributeError as e:
+            return type(e).__name__ + ': Column %s does not exists' % (e.name)
+        
+        return lenPrecision
+
+    def getDataType(column_name):
+
+        try:
+            data_type = getattr(CustomerEntity, column_name).get('data_type')
+
+        except AttributeError as e:
+            return type(e).__name__ + ': Column %s does not exists' % (e.name)
+        
+        return data_type
+    
+    def columnIsPK(column_name):
+    
+        try:
+            primary_key = getattr(CustomerEntity, column_name).get('primary_key')
+
+        except AttributeError as e:
+            return type(e).__name__ + ': Column %s does not exists' % (e.name)
+
+        return False if primary_key == None else primary_key
