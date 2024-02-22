7a9c1c7ae309887b7f3c369ea1bfdbc2fa440796
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2024-02-21 21:30:30 -0300

    feat: error handler - client not found with the provided document

--- src/repositories/CustomerRepository.py
+++ src/repositories/CustomerRepository.py
@@ -1,21 +1,28 @@
 from ..adapters.CustomerAdapter import CustomerAdapter
 from ..resources.dbconfig import Session
-from flask import jsonify
+from flask import jsonify, abort
 
 class CustomerRepository():
 
     def findCustomerByDocument(document):
 
-        session = Session()
+        session = Session()   
         customer_query = session.query(CustomerAdapter).filter_by(document=document).first()
-        customer_data = jsonify({
-            'id_customer': customer_query.id_customer,
-            'first_name': customer_query.first_name,
-            'last_name': customer_query.last_name,
-            'address': customer_query.address,
-            'phone_number': customer_query.phone_number,
-            'document': customer_query.document,
-            'birthdate': customer_query.birthdate
-        })
+        
+        if customer_query == None:
 
-        return customer_data
+            return abort(404, 'None customer found with the provided document')
+
+        else:
+
+            customer_data = jsonify({
+                'id_customer': customer_query.id_customer,
+                'first_name': customer_query.first_name,
+                'last_name': customer_query.last_name,
+                'address': customer_query.address,
+                'phone_number': customer_query.phone_number,
+                'document': customer_query.document,
+                'birthdate': customer_query.birthdate
+            })
+
+            return customer_data
