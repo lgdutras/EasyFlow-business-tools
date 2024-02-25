d221055391fde093a001fb7baf4440e054e321df
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2024-02-25 01:02:34 -0300

    chore: Create Super Entity to reuse the Entity methods on all entitys on Orders domain

--- src/domain/orders/Order.py
+++ src/domain/orders/Order.py
@@ -1,19 +1,10 @@
-class OrderEntity():
-    # Meta Data
-    table_name = 'orders'
+class Entity():
 
-    # Columns
-    id_order = {'column_name': 'id_order', 'object_type': 'Column', 'data_type': 'Integer', 'primary_key': True}
-    order_status = {'column_name': 'order_status', 'object_type': 'Column', 'data_type': 'Integer'}
-    id_receipt = {'column_name': 'id_receipt', 'object_type': 'Column', 'data_type': 'Integer'}
-    id_customer = {'column_name': 'id_customer', 'object_type': 'Column', 'data_type': 'Integer'}
-
-    
-    def getLenPrecision(column_name):
+    def getLenPrecision(self, column_name):
 
         try:
-            lenght = int(0 if getattr(OrderEntity, column_name).get('length') == None else getattr(OrderEntity, column_name).get('length'))
-            precision = int(0 if getattr(OrderEntity, column_name).get('precision') == None else getattr(OrderEntity, column_name).get('precision'))
+            length = int(0 if getattr(self, column_name).get('length') == None else getattr(self, column_name).get('length'))
+            precision = int(0 if getattr(self, column_name).get('precision') == None else getattr(self, column_name).get('precision'))
 
             lenPrecision = '%s,%s' % (len, precision) if (precision != None and precision > 0) else len if precision != None else None
 
@@ -22,22 +13,46 @@ class OrderEntity():
         
         return lenPrecision
 
-    def getDataType(column_name):
+    def getDataType(self, column_name):
 
         try:
-            data_type = getattr(OrderEntity, column_name).get('data_type')
+            data_type = getattr(self, column_name).get('data_type')
 
         except AttributeError as e:
             return type(e).__name__ + ': Column %s does not exists' % (e.name)
         
         return data_type
     
-    def columnIsPK(column_name):
+    def columnIsPK(self, column_name):
     
         try:
-            primary_key = getattr(OrderEntity, column_name).get('primary_key')
+            primary_key = getattr(self, column_name).get('primary_key')
 
         except AttributeError as e:
             return type(e).__name__ + ': Column %s does not exists' % (e.name)
 
         return False if primary_key == None else primary_key
+
+class OrderEntity(Entity):
+
+    # Meta Data
+    table_name = 'orders'
+
+    # Columns
+    id_order = {'column_name': 'id_order', 'object_type': 'Column', 'data_type': 'Integer', 'primary_key': True}
+    order_status = {'column_name': 'order_status', 'object_type': 'Column', 'data_type': 'Integer'}
+    id_receipt = {'column_name': 'id_receipt', 'object_type': 'Column', 'data_type': 'Integer'}
+    id_customer = {'column_name': 'id_customer', 'object_type': 'Column', 'data_type': 'Integer'}
+    
+
+class OrderItemEntity(Entity):
+
+    # Meta Data
+    table_name = 'order_product'
+
+    # Columns
+    id_order = {'column_name': 'id_order', 'object_type': 'Column', 'data_type': 'Integer', 'primary_key': True}
+    id_product = {'column_name': 'id_product', 'object_type': 'Column', 'data_type': 'Integer'}
+    price = {'column_name': 'price', 'object_type': 'Column', 'data_type': 'Decimal', 'length': '8', 'precision': '4'}
+    quantity = {'column_name': 'quantity', 'object_type': 'Column', 'data_type': 'Integer'}
+    datetime = {'column_name': 'datetime', 'object_type': 'Column', 'data_type': 'Datetime'}
